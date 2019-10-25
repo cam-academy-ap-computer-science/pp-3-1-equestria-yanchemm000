376686f6925cfe4939c65d0ccab86e987b33c5af
yanchemm000 <yanchemm000@CAM-LAB-27.bgsd.k12.wa.us>
2019-10-25 07:53:48 -0700

    need to figure this out

--- src/Equestria.java
+++ src/Equestria.java
@@ -2,8 +2,27 @@
 public class Equestria {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		 int BaltX = 31;
+		 int BaltY = 15;
+		 int ManeX = 35;
+		 int ManeY = 9;
+		 
+		 double BalttoManeX = XDistance(BaltX, ManeX);
+		 double BalttoManeY = YDistance(BaltY, ManeY);
+		 
+		 System.out.println(BaltX);
+	}
+	public static double XDistance ( double X1, double X2) {
+		double X = X2 - X1;
+		return X;
+	}
+	public static double YDistance ( double Y1, double Y2) {
+		double Y = Y2 - Y1;
+		return Y;
+	}
+	public static double Squared (double X, double Y) {
+		Math.class.sqrt (X);
+		return X;
 	}
 
 }
